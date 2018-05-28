c54f138c8651eb705266d5018627e96fb2a88c9a
Richard Bromley <bromley@unlv.nevada.edu>
2018-05-27 17:38:05 -0700

    Code to Minimum synchronous read/write

--- client.cs
+++ client.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Threading;
 using System.Net.Sockets;
 
 class EmployeeTCPClient{
@@ -12,15 +13,10 @@ class EmployeeTCPClient{
             StreamWriter sw = new StreamWriter(s);
             sw.AutoFlush = true;
             Console.WriteLine(sr.ReadLine());
-            Console.Write("Connected to Dick Bromley");
-            while(true){
-
-                string name = Console.ReadLine();
-                sw.WriteLine(name);
-                if(name == "") break;
-                Console.WriteLine(sr.ReadLine());
-            }
-            s.Close();
+            Console.WriteLine("Connected to 172.16.12.102");
+            Thread x = new Thread(new ParameterizedThreadStart(recieve));
+            x.Start(s);
+            while(true){ sw.WriteLine(Console.ReadLine());}
         }finally{
             // code in finally block is guranteed 
             // to execute irrespective of 
@@ -28,5 +24,10 @@ class EmployeeTCPClient{
             // not occur in the try block
             client.Close();
         } 
+    }
+/*************************************************************************************/
+    public static void recieve(object s){ //must be an object then cast!
+                StreamReader sr = new StreamReader((Stream) s);
+             while(true) { Console.WriteLine(sr.ReadLine()); }
     }
 }
--- server.cs
+++ server.cs
@@ -18,7 +18,6 @@ class Communicator{
     }
      /*************************************************************************************/
     public static void Service(){
-        while(true){
 				Socket soc = listener.AcceptSocket();
                 Console.WriteLine("Connected: {0}", soc.RemoteEndPoint);
             try{
@@ -32,13 +31,9 @@ class Communicator{
                 x.Start(s);
 
                 while(true){ sw.WriteLine(Console.ReadLine());}
-                s.Close();
             }catch(Exception e){
 				Console.WriteLine(e.Message);
             }
-            Console.WriteLine("Disconnected: {0}", soc.RemoteEndPoint);
-            soc.Close();
-        }
     }
     /*************************************************************************************/
     public static void connector(){
@@ -46,11 +41,9 @@ class Communicator{
         t.Start();
     }
     /*************************************************************************************/
-    public static void recieve(object s){ //must be an object then cast!
+    public static void recieve(object s){ // Must be an object then cast! 
+                                          // Synchronous read / write
                 StreamReader sr = new StreamReader((Stream) s);
-             while(true) {
-                string name = sr.ReadLine();
-                Console.WriteLine("{0}",name);
-             }
+             while(true) { Console.WriteLine(sr.ReadLine());}
     }
 }
